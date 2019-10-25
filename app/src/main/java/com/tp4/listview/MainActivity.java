ba18b94d13b920f8de0611799ac2fa3702d90fe0
sushantmzjn <[sushantmzjn@gmail.com]>
2019-10-25 10:24:04 +0545

    dd

--- app/src/main/java/com/tp4/listview/MainActivity.java
+++ app/src/main/java/com/tp4/listview/MainActivity.java
@@ -3,12 +3,28 @@ package com.tp4.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView country;
+
+
+    public static final String countries[]={"Nepal", "China", "India"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        country = findViewById(R.id.list);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,countries);
+
+
+        country.setAdapter(adapter);
+
     }
 }
